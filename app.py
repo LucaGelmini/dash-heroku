8ec1004695f76d32ba73a04a88d57345e5b09863
LucaGelmini <lucagelmini@hotmail.com>
2022-06-06 13:56:18 -0300

    grafico pruebita

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from dash import Dash, callback, html, dcc
+from dash.dependencies import Output
 import dash_bootstrap_components as dbc
 import numpy as np
 import pandas as pd
 import matplotlib as mpl
 import gunicorn                     #whilst your local machine's webserver doesn't need this, Heroku's linux webserver (i.e. dyno) does. I.e. This is your HTTP server
 from whitenoise import WhiteNoise   #for serving static files on Heroku
+from plotly import graph_objects as go
 
 # Instantiate dash app
 app = Dash(__name__, external_stylesheets=[dbc.themes.FLATLY])
@@ -14,6 +16,21 @@ server = app.server
 
 # Enable Whitenoise for serving static files from Heroku (the /static folder is seen as root by Heroku) 
 server.wsgi_app = WhiteNoise(server.wsgi_app, root='static/') 
+
+
+df_cif = pd.read_json('data/CIF_PU(2019-2022).json')
+df_fob = pd.read_json('./data/FOB_PU(2019-2022).json')
+def series(df, cnomen, cpais):
+    meses = ['ENE','FEB','MAR','ABR','MAY','JUN','JUL','AGO','SEP','OCT','NOV','DIC']
+    return df[(df.CNOMEN == cnomen) & (df.CPAIS == cpais)][meses].iloc[0]
+def series_multianios(df, cnomen, cpais, desde, hasta):
+    s = []
+    intervalo = list(range(desde, hasta+1, 1))
+    for i, anio in enumerate(intervalo):
+        s.append(series(df[df.CANIO==anio], cnomen, cpais))
+        s[i].index = s[i].index +f'-{anio}'
+    return pd.concat(s)
+
 
 # Define Dash layout
 def create_dash_layout(app):
@@ -25,7 +42,13 @@ def create_dash_layout(app):
     header = html.Div([html.Br(), dcc.Markdown(""" # Hi. I'm your Dash app."""), html.Br()])
     
     # Body 
-    body = html.Div([dcc.Markdown(""" ## I'm ready to serve static files on Heroku. Just look at this! """), html.Br(), html.Img(src='charlie.png')])
+    body = html.Div([
+        dcc.Markdown(""" ## I'm ready to serve static files on Heroku. Just look at this! """),
+        html.Br(),
+        html.Img(src='charlie.png'),
+        html.Br(),
+        dcc.Graph(id='pruebita')
+        ])
 
     # Footer
     footer = html.Div([html.Br(), html.Br(), dcc.Markdown(""" ### Built with ![Image](heart.png) in Python using [Dash](https://plotly.com/dash/)""")])
@@ -34,6 +57,19 @@ def create_dash_layout(app):
     app.layout = html.Div([header, body, footer])
 
     return app
+
+@app.callback(
+    Output('pruebita', 'figure')
+)
+def update_chart():
+    x=[1,2,3]
+    y=[3,5,2]
+    
+    fig= go.Figure()
+    fig.add_trace(go.Scatter(x=x, y=y))
+    
+    return fig
+    
 
 # Construct the dash layout
 create_dash_layout(app)
